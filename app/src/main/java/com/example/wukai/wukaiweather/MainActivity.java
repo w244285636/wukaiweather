4bd8ad8341c6702862a9a28fef1e663178232232
wukai <wukai6038@163.com>
2017-05-26 15:45:35 +0800

    完成遍历省市县三级列表遍历的功能

--- app/src/main/java/com/example/wukai/wukaiweather/MainActivity.java
+++ app/src/main/java/com/example/wukai/wukaiweather/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.wukai.wukaiweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("wukai","111111111");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
---
+++ app/src/main/java/com/example/wukai/wukaiweather/util/Utility.java
@@ -0,0 +1,81 @@
+package com.example.wukai.wukaiweather.util;
+
+import android.text.TextUtils;
+
+import com.example.wukai.wukaiweather.db.City;
+import com.example.wukai.wukaiweather.db.County;
+import com.example.wukai.wukaiweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by wukai on 2017/5/25.
+ * 这里是为了处理服务器返回的JSON数据，对其进行解析和处理。
+ */
+
+public class Utility {
+
+    public static boolean handleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0;i < allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response,int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities =  new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountyResponse(String response,int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0; i < allCounties.length();i++){
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+
+}
